26f56408c965bdafa7e48c8f02d0bf77b38f91ce
Rachael Blake <rblake@rstudio03.research.sesync.org>
2020-08-20 16:32:59 -0400

    comment out useless diffs

--- diff_files.R
+++ diff_files.R
@@ -1,5 +1,5 @@
 #################
-###  Diff between Rebecca Turner's taxonomy file and the authoritative taxononmy table
+###  Diff between Rebecca Turner's taxonomy file and the authoritative taxonomy table
 ################
 
 library(diffobj) ; library(tidyverse) ; library(compareDF)
@@ -15,32 +15,33 @@ tax_table <- tax_table %>% arrange(taxon_id)
 nrow(rt_aug_file) ; nrow(tax_table)  # 2 rows difference
 names(rt_aug_file) ; names(tax_table)
 
+################################################
 # Diff between the two files:
 
-diffPrint(target = rt_aug_file, current = tax_table)
-
-# only returns two rows of differences, but unintelligable result
+# diffPrint(target = rt_aug_file, current = tax_table)
+# 
+# # only returns two rows of differences, but unintelligible result
 
 ################################################
 
 # Use another method to Diff the two files: 
 
-diff1 <- setdiff(rt_aug_file, tax_table)
+diff1 <- setdiff(tax_table, rt_aug_file)
 str(diff1)
 
 ################################################
 
-# Use another method to Diff the two files: 
-
-dplyr::all_equal(tax_table, rt_aug_file)  # not useful!  already get this from nrow() above!
+# # Use another method to Diff the two files: 
+# 
+# dplyr::all_equal(tax_table, rt_aug_file)  # not useful!  already get this from nrow() above!
 
 
 ################################################
 
-# Use another method to Diff the two files: 
-
-diff2 <- compare_df(tax_table, rt_aug_file, c("taxon_id"))
-
+# # Use another method to Diff the two files: 
+# 
+# diff2 <- compare_df(tax_table, rt_aug_file, c("taxon_id"))
+# 
 
 ################################################
 
@@ -48,7 +49,7 @@ diff2 <- compare_df(tax_table, rt_aug_file, c("taxon_id"))
 
 tax_table[!(tax_table$taxon_id %in% rt_aug_file$taxon_id),]   # Finally something that's useful!!
 
-anti_join(tax_table, rt_aug_file, by = "taxon_id")
+anti_join(tax_table, rt_aug_file, by = "taxon_id")  # returns same as line above
 
-diff3 <- anti_join(tax_table, rt_aug_file)
+diff3 <- anti_join(tax_table, rt_aug_file)  # returns 989 differences
 
