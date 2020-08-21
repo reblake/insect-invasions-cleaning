bf47162ff2920a773783dc28fb4647408f080b84
Rachael Blake <rblake@rstudio03.research.sesync.org>
2020-08-21 16:36:10 -0400

    convert diff_files to Rmd and knit to html

--- diff_files.R
+++ diff_files.R
@@ -2,7 +2,7 @@
 ###  Diff between Rebecca Turner's taxonomy file and the authoritative taxonomy table
 ################
 
-library(diffobj) ; library(tidyverse) ; library(compareDF)
+library(tidyverse) ; library(testthat)
 
 # Rebecca's file
 rt_aug_file <- read.csv("nfs_data/data/clean_data/taxonomy_table2020Aug12b.csv")
@@ -18,38 +18,49 @@ names(rt_aug_file) ; names(tax_table)
 ################################################
 # Diff between the two files:
 
-# diffPrint(target = rt_aug_file, current = tax_table)
-# 
-# # only returns two rows of differences, but unintelligible result
-
-################################################
-
-# Use another method to Diff the two files: 
-
 diff1 <- setdiff(tax_table, rt_aug_file)
 str(diff1)
 
 ################################################
 
-# # Use another method to Diff the two files: 
-# 
-# dplyr::all_equal(tax_table, rt_aug_file)  # not useful!  already get this from nrow() above!
-
-
-################################################
-
-# # Use another method to Diff the two files: 
-# 
-# diff2 <- compare_df(tax_table, rt_aug_file, c("taxon_id"))
-# 
-
-################################################
-
 # Use another method to Diff the two files:
 
-tax_table[!(tax_table$taxon_id %in% rt_aug_file$taxon_id),]   # Finally something that's useful!!
+tax_table[!(tax_table$taxon_id %in% rt_aug_file$taxon_id),]   # something that's useful!!
 
 anti_join(tax_table, rt_aug_file, by = "taxon_id")  # returns same as line above
 
-diff3 <- anti_join(tax_table, rt_aug_file)  # returns 899 differences
+diff3 <- anti_join(tax_table, rt_aug_file)  # returns differences
 str(diff3)
+
+#####
+# I suspect that taxa are being numbered differently between the two cleaning codes, so testing that here
+tax_table2 <- tax_table %>% mutate(id_usr_supp_nm = paste(taxon_id, user_supplied_name))
+
+rt_aug_file2 <- rt_aug_file %>% mutate(id_usr_supp_nm = paste(taxon_id, user_supplied_name))
+
+# compare based on the two pasted columns
+diff4 <- anti_join(tax_table2, rt_aug_file2, by = "id_usr_supp_nm") # rows of tax_table2 that don't have a match in rt_aug_file2
+str(diff4)
+
+diff5 <- anti_join(rt_aug_file2, tax_table2, by = "id_usr_supp_nm") # rows of rt_aug_file2 that don't have a match in tax_table2
+str(diff5)
+
+#####
+# Subset diff3 to remove rows with taxon_id mismatches
+other_probs <- anti_join(diff3, diff4) %>% arrange(user_supplied_name) %>% 
+               
+
+usr_nms_diff <- other_probs$user_supplied_name
+
+rt_diff <- rt_aug_file %>% filter(user_supplied_name %in% usr_nms_diff) %>% 
+           arrange(user_supplied_name)
+
+# This is actually quite useful!  Gives a print out of which columns have issues and how many issues.
+testthat::expect_equal(other_probs, rt_diff)   
+
+# further investigation based on testthat results
+tt_status_na <- other_probs[is.na(other_probs$status), ]
+rt_status_na <- rt_diff[is.na(rt_diff$status), ]
+anti_join(tt_status_na, rt_status_na, by = "user_supplied_name")
+
+
