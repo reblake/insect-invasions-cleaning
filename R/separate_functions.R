1ddc36f157f75250b0dcc17535a5204f22c55616
Rachael Blake <rblake@sesync.org@sesync00.research.sesync.org>
2022-01-31 16:46:52 -0500

    udpate package guts

--- R/separate_functions.R
+++ R/separate_functions.R
@@ -71,10 +71,11 @@ separate_taxonomy_xl <- function(df_location){
 ######################
 #' separate_taxonomy_csv: a function that cleans the dataframes and separates taxonomy columns using csv files
 #'
-#' @param df_location
+#' @param df_location location of files containing raw insect data
 #'
 #' @import dplyr
 #' @importFrom tidyselect one_of
+#' @importFrom utils read.csv
 #'
 #' @return dataframe
 #' @export
@@ -218,6 +219,7 @@ separate_occurrence_xl <- function(df_location){
 #' @param df_location location of files containing taxa info
 #'
 #' @import dplyr
+#' @importFrom utils read.csv
 #'
 #' @return dataframe
 #' @export
@@ -372,6 +374,7 @@ separate_attributes_xl <- function(df_location){
 #' @param df_location location of file containing taxa info
 #'
 #' @import dplyr
+#' @importFrom utils read.csv
 #'
 #' @return dataframe
 #' @export
@@ -382,61 +385,62 @@ separate_attributes_xl <- function(df_location){
 #' attrib <- separate_attributes_csv(my_df)
 #' }
 separate_attributes_csv <- function(df_location){
-  # reads the excel file in
-  df <- read.csv(df_location, strip.white = TRUE)
-  
-  # clean up column names, capitalization, etc.
-  df_1 <- df %>%
-    # replace " " and "." with "_" in column names
-    select_all(~gsub("\\s+|\\.", "_", .)) %>%
-    select_all(tolower) %>%  # make all column names lower case
-    mutate_all(~gsub("\\b([[:upper:]])([[:upper:]]+)",
-                     "\\U\\1\\L\\2", . , perl=TRUE))
-  
-  # define region
-  file_name <-  str_extract(df_location, "[^\\/]+$")
-  country_nm <-  str_extract(file_name, "^[^\\_]+")
-  
-  df_2 <- df_1 %>%
-    # split off any columns that are not relevant
-    select(-one_of("kingdom", "phylum", "class", "order", "family",
-                   "genus", "species", "authority", "super_family",
-                   "suborder", "author", "common_name", "taxonomy_system",
-                   "jp_name", "source", "reference", "status", "synonym",
-                   "origin2", "tsn", "comment", "original_species_name",
-                   "rank", "name_changed___1_yes__0__no_", "size_mm_",
-                   "town", "rege_date_source", "nz_area_code", "life_form",
-                   "data_quality", "year", "canada_or_us"
-    )) %>%
-    mutate_all(~gsub("(*UCP)\\s\\+|\\W+$", "", . , perl=TRUE)) %>%
-    # clean up the taxonomic names
-    mutate(genus_species = gsub("\\ssp\\s[a-z]+\\s[a-z]+$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\ssp\\s[A-Za-z]+\\d+$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\ssp\\.\\d\\s\\s[A-Za-z]+$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\s[A-Z]\\.[A-Z]\\.[A-Z][a-z]+\\,\\s\\d+$", "", genus_species, perl=TRUE),
-           genus_species = gsub("[^\x20-\x7E]sp", "", genus_species, perl=TRUE),
-           genus_species = gsub("[^\x20-\x7E]", " ", genus_species, perl=TRUE),
-           genus_species = gsub("\\s\\([^()]*\\)", "\\1", genus_species, perl=TRUE),
-           genus_species = gsub("\\([A-Z].*$", "\\1", genus_species, perl=TRUE),
-           genus_species = gsub("^([A-Z][a-z]+\\s\\S+).*", "\\1", genus_species, perl=TRUE),
-           genus_species = gsub("\\sssp\\.\\s[a-z].*$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\ssp\\.[A-Z]$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\ssp[A-Z]$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\ssp(\\.|p|\\d|\\.\\d)$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\.", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\sn\\.sp\\.$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\sn$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\s\\ssp$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\d+$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\s\\ss", " ", genus_species, perl=TRUE),
-           genus_species = gsub("\\s\\s", " ", genus_species, perl=TRUE),
-           genus_species = gsub("\\ssp$", "", genus_species, perl=TRUE)
-    ) %>%
-    # add country_nm column
-    mutate(country_nm = country_nm)
-  
-  return(df_2)
-}
+                           # reads the excel file in
+                           df <- read.csv(df_location, strip.white = TRUE)
+                           # clean up column names, capitalization, etc.
+                           df_1 <- df %>%
+                                   # replace " " and "." with "_" in column names
+                                   select_all(~gsub("\\s+|\\.", "_", .)) %>%
+                                   select_all(tolower) %>%  # make all column names lower case
+                                   mutate_all(~gsub("\\b([[:upper:]])([[:upper:]]+)",
+                                                    "\\U\\1\\L\\2", . , perl=TRUE))
+
+                           # define region
+                           file_name <-  str_extract(df_location, "[^\\/]+$")
+                           country_nm <-  str_extract(file_name, "^[^\\_]+")
+
+                           df_2 <- df_1 %>%
+                                   # split off any columns that are not relevant
+                                   select(-one_of("kingdom", "phylum", "class", "order", "family",
+                                                  "genus", "species", "authority", "super_family",
+                                                  "suborder", "author", "common_name", "taxonomy_system",
+                                                  "jp_name", "source", "reference", "status", "synonym",
+                                                  "origin2", "tsn", "comment", "original_species_name",
+                                                  "rank", "name_changed___1_yes__0__no_", "size_mm_",
+                                                  "town", "rege_date_source", "nz_area_code", "life_form",
+                                                  "data_quality", "year", "canada_or_us"
+                                                  )) %>%
+                                   mutate_all(~gsub("(*UCP)\\s\\+|\\W+$", "", . , perl=TRUE)) %>%
+                           # clean up the taxonomic names
+                           mutate(genus_species = gsub("\\ssp\\s[a-z]+\\s[a-z]+$", "", genus_species, perl=TRUE),
+                                  genus_species = gsub("\\ssp\\s[A-Za-z]+\\d+$", "", genus_species, perl=TRUE),
+                                  genus_species = gsub("\\ssp\\.\\d\\s\\s[A-Za-z]+$", "", genus_species, perl=TRUE),
+                                  genus_species = gsub("\\s[A-Z]\\.[A-Z]\\.[A-Z][a-z]+\\,\\s\\d+$", "", genus_species, perl=TRUE),
+                                  genus_species = gsub("[^\x20-\x7E]sp", "", genus_species, perl=TRUE),
+                                  genus_species = gsub("[^\x20-\x7E]", " ", genus_species, perl=TRUE),
+                                  genus_species = gsub("\\s\\([^()]*\\)", "\\1", genus_species, perl=TRUE),
+                                  genus_species = gsub("\\([A-Z].*$", "\\1", genus_species, perl=TRUE),
+                                  genus_species = gsub("^([A-Z][a-z]+\\s\\S+).*", "\\1", genus_species, perl=TRUE),
+                                  genus_species = gsub("\\sssp\\.\\s[a-z].*$", "", genus_species, perl=TRUE),
+                                  genus_species = gsub("\\ssp\\.[A-Z]$", "", genus_species, perl=TRUE),
+                                  genus_species = gsub("\\ssp[A-Z]$", "", genus_species, perl=TRUE),
+                                  genus_species = gsub("\\ssp(\\.|p|\\d|\\.\\d)$", "", genus_species, perl=TRUE),
+                                  genus_species = gsub("\\.", "", genus_species, perl=TRUE),
+                                  genus_species = gsub("\\sn\\.sp\\.$", "", genus_species, perl=TRUE),
+                                  genus_species = gsub("\\sn$", "", genus_species, perl=TRUE),
+                                  genus_species = gsub("\\s\\ssp$", "", genus_species, perl=TRUE),
+                                  genus_species = gsub("\\d+$", "", genus_species, perl=TRUE),
+                                  genus_species = gsub("\\s\\ss", " ", genus_species, perl=TRUE),
+                                  genus_species = gsub("\\s\\s", " ", genus_species, perl=TRUE),
+                                  genus_species = gsub("\\ssp$", "", genus_species, perl=TRUE)
+                                  ) %>%
+                           # add country_nm column
+                           mutate(country_nm = country_nm)
+
+                           return(df_2)
+                           }
+
+
 
 ###########################################################
 #' separate_source_csv: function to read in and separate source info using csv files
@@ -454,56 +458,58 @@ separate_attributes_csv <- function(df_location){
 #' }
 
 separate_source_csv <- function(df_location){
-  # reads the csv file in
-  df <- read.csv(df_location, strip.white = TRUE)
-  
-  # clean up column names, capitalization, etc.
-  df_1 <- df %>%
-    # replace " " and "." with "_" in column names
-    select_all(~gsub("\\s+|\\.", "_", .)) %>%
-    select_all(tolower) %>%  # make all column names lower case
-    mutate_all(~gsub("\\b([[:upper:]])([[:upper:]]+)",
-                     "\\U\\1\\L\\2", . , perl=TRUE))
-  
-  # define region
-  file_name <-  str_extract(df_location, "[^\\/]+$")
-  country_nm <-  str_extract(file_name, "^[^\\_]+")
-  
-  # define what relevant columns to select - note that the original column caled genus_species will become the user_supplied_name column
-  source_columns <- c("genus_species", "year","intentional_release","eradicated","established_indoors_or_outdoors","source")
-  
-  # split off any columns with any taxonomic column names
-  df_2 <- df_1 %>%
-    select(tidyselect::one_of(source_columns)) %>%
-    # add the name of the country as a column
-    mutate(region = country_nm) %>%
-    mutate_all(~gsub("(*UCP)\\s\\+|\\W+$", "", . , perl=TRUE)) %>%
-    # replace any non-numerical values in year column with NA
-    mutate(year = gsub("u", NA_character_, year, perl=TRUE)) %>%
-    mutate(genus_species = gsub("\\ssp\\s[a-z]+\\s[a-z]+$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\ssp\\s[A-Za-z]+\\d+$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\ssp\\.\\d\\s\\s[A-Za-z]+$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\s[A-Z]\\.[A-Z]\\.[A-Z][a-z]+\\,\\s\\d+$", "", genus_species, perl=TRUE),
-           genus_species = gsub("[^\x20-\x7E]sp", "", genus_species, perl=TRUE),
-           genus_species = gsub("[^\x20-\x7E]", " ", genus_species, perl=TRUE),
-           genus_species = gsub("\\s\\([^()]*\\)", "\\1", genus_species, perl=TRUE),
-           genus_species = gsub("\\([A-Z].*$", "\\1", genus_species, perl=TRUE),
-           genus_species = gsub("^([A-Z][a-z]+\\s\\S+).*", "\\1", genus_species, perl=TRUE),
-           genus_species = gsub("\\sssp\\.\\s[a-z].*$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\ssp\\.[A-Z]$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\ssp[A-Z]$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\ssp(\\.|p|\\d|\\.\\d)$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\.", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\sn\\.sp\\.$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\sn$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\s\\ssp$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\d+$", "", genus_species, perl=TRUE),
-           genus_species = gsub("\\s\\ss", " ", genus_species, perl=TRUE),
-           genus_species = gsub("\\s\\s", " ", genus_species, perl=TRUE),
-           genus_species = gsub("\\ssp$", "", genus_species, perl=TRUE)
-    )
-  
-  # return df_2
-  return(df_2)
-}
+                       # reads the csv file in
+                       df <- read.csv(df_location, strip.white = TRUE)
+
+                       # clean up column names, capitalization, etc.
+                       df_1 <- df %>%
+                               # replace " " and "." with "_" in column names
+                               select_all(~gsub("\\s+|\\.", "_", .)) %>%
+                               select_all(tolower) %>%  # make all column names lower case
+                               mutate_all(~gsub("\\b([[:upper:]])([[:upper:]]+)",
+                                                "\\U\\1\\L\\2", . , perl=TRUE))
+
+                       # define region
+                       file_name <-  str_extract(df_location, "[^\\/]+$")
+                       country_nm <-  str_extract(file_name, "^[^\\_]+")
+
+                       # define what relevant columns to select - note that the original column
+                       # called genus_species will become the user_supplied_name column
+                       source_columns <- c("genus_species", "year","intentional_release","eradicated",
+                                           "established_indoors_or_outdoors","source")
+
+                       # split off any columns with any taxonomic column names
+                       df_2 <- df_1 %>%
+                               select(tidyselect::one_of(source_columns)) %>%
+                               # add the name of the country as a column
+                               mutate(region = country_nm) %>%
+                               mutate_all(~gsub("(*UCP)\\s\\+|\\W+$", "", . , perl=TRUE)) %>%
+                               # replace any non-numerical values in year column with NA
+                               mutate(year = gsub("u", NA_character_, year, perl=TRUE)) %>%
+                               mutate(genus_species = gsub("\\ssp\\s[a-z]+\\s[a-z]+$", "", genus_species, perl=TRUE),
+                                      genus_species = gsub("\\ssp\\s[A-Za-z]+\\d+$", "", genus_species, perl=TRUE),
+                                      genus_species = gsub("\\ssp\\.\\d\\s\\s[A-Za-z]+$", "", genus_species, perl=TRUE),
+                                      genus_species = gsub("\\s[A-Z]\\.[A-Z]\\.[A-Z][a-z]+\\,\\s\\d+$", "", genus_species, perl=TRUE),
+                                      genus_species = gsub("[^\x20-\x7E]sp", "", genus_species, perl=TRUE),
+                                      genus_species = gsub("[^\x20-\x7E]", " ", genus_species, perl=TRUE),
+                                      genus_species = gsub("\\s\\([^()]*\\)", "\\1", genus_species, perl=TRUE),
+                                      genus_species = gsub("\\([A-Z].*$", "\\1", genus_species, perl=TRUE),
+                                      genus_species = gsub("^([A-Z][a-z]+\\s\\S+).*", "\\1", genus_species, perl=TRUE),
+                                      genus_species = gsub("\\sssp\\.\\s[a-z].*$", "", genus_species, perl=TRUE),
+                                      genus_species = gsub("\\ssp\\.[A-Z]$", "", genus_species, perl=TRUE),
+                                      genus_species = gsub("\\ssp[A-Z]$", "", genus_species, perl=TRUE),
+                                      genus_species = gsub("\\ssp(\\.|p|\\d|\\.\\d)$", "", genus_species, perl=TRUE),
+                                      genus_species = gsub("\\.", "", genus_species, perl=TRUE),
+                                      genus_species = gsub("\\sn\\.sp\\.$", "", genus_species, perl=TRUE),
+                                      genus_species = gsub("\\sn$", "", genus_species, perl=TRUE),
+                                      genus_species = gsub("\\s\\ssp$", "", genus_species, perl=TRUE),
+                                      genus_species = gsub("\\d+$", "", genus_species, perl=TRUE),
+                                      genus_species = gsub("\\s\\ss", " ", genus_species, perl=TRUE),
+                                      genus_species = gsub("\\s\\s", " ", genus_species, perl=TRUE),
+                                      genus_species = gsub("\\ssp$", "", genus_species, perl=TRUE)
+                                      )
+
+                       # return df_2
+                       return(df_2)
+                       }
 
